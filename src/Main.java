d8849cbdc1562c014abe5fccfedd91951a612187
yuval <malkayuval.1711@gmail.com>
2024-01-11 15:07:08 +0200

    haide added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("nicol + yuval");
-        System.out.println("= love");
+        System.out.println("= love + haide");
     }
 }
